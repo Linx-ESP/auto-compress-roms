2947173b40b51a9cabf189b5eb3ae995252c9ec9
Juan Herrero <51830853+Linx-ESP@users.noreply.github.com>
2023-02-04 21:12:46 +0100

    Update ps2-opl-zso.sh

--- bash-scripts/ps2-opl-zso.sh
+++ bash-scripts/ps2-opl-zso.sh
@@ -1,1 +1,7 @@
-for f in (import folder)/*.iso; do "(path to)/opl-ziso.py" -c 2 "$f" "(path to)/OpenPS2Loader/DVD/$(basename "$f" .iso).zso"; rm "$f"; done
+# Use quotes ("")
+import_dir="/path/original/files"
+zso_py_dir="/path/for/script" # MUST include /opl-ziso.py or /ziso.py or what name it has.
+opl_dvd_dir="/path/for/OpenPS2Loader" # Should end in /OpenPS2Loader if you use SMB in OPL. In no case in /DVD or /CD
+
+# Dont need to edit under this line
+for f in "$import_dir"/*.iso; do "$zso_py_dir" -c 2 "$f" "$opl_dvd_dir/DVD/$(basename "$f" .iso).zso"; rm "$f"; done
