a3617962f4e745a3999b998f7dc262916083590e
Juan Herrero <51830853+Linx-ESP@users.noreply.github.com>
2023-02-04 21:56:48 +0100

    Update wiicompress.sh

--- bash-scripts/wiicompress.sh
+++ bash-scripts/wiicompress.sh
@@ -9,6 +9,7 @@ wbfs_dir="/path/for/wbfs"
 cd "$import_dir"; unp *.*;      # Unpack all SHOULD do only unpackable formats and error if no dependencies should not stop the script.
 
 # Nkit extract TO-DO
+# Conversion to rvz requires full Dolphin (not doable in headless server?)
 
 for f in *.iso; do wit convert --wbfs "$f" .; done;       # Keeps the .wbfs in same place, move everything at the same time later
 
