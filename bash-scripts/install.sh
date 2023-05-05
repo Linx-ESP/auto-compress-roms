d30556afb132d13fd3b4436499569a452a744fa7
Juan Herrero <51830853+Linx-ESP@users.noreply.github.com>
2023-05-05 18:29:35 +0200

    Update install.sh

--- bash-scripts/install.sh
+++ bash-scripts/install.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get install -y ansible
 
 # Download Ansible playbook from URL
-curl -L -o playbook.yml https://github.com/Linx-ESP/auto-compress-roms/blob/ansible-test1/ansible/install-dependencies.yaml
+curl -L -o playbook.yml https://raw.githubusercontent.com/Linx-ESP/auto-compress-roms/ansible-test1/ansible/install-dependencies.yaml
 
 # Run Ansible playbook
 ansible-playbook playbook.yml
