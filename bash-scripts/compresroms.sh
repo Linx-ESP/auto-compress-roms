a92c124790c4ebb0e895e662666aacbd704a8ef6
Linx <51830853+Linx-ESP@users.noreply.github.com>
2023-05-09 18:35:16 +0200

    Now with main and system-wide executable script

---
+++ bash-scripts/compresroms.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ================ Should point to help =================
+if [[ "$1" == "" ]]; then
+    echo "No argument. Please specify 'gamecube' , 'wii', 'ps2'."
+
+# ================== Nintendo Consoles ==================
+elif [[ "$1" == "gamecube" ]]; then
+    bash /media/games/scripts/gc-iso2ciso.sh
+
+elif [[ "$1" == "wii" ]]; then
+    bash /media/games/scripts/wii-iso2wbfs.sh
+
+# ==================== Sony Consoles ====================
+elif [[ "$1" == "ps2" ]]; then
+    bash /media/games/scripts/ps2-opl-iso2zso.sh
+
+# ======================== Error ========================
+else
+    echo "Invalid argument."
+fi
