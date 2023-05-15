1a77ab9b022218be7777f827244a765660eceb9f
Linx <51830853+Linx-ESP@users.noreply.github.com>
2023-05-15 13:37:24 +0200

    Change CRLF to LF Add paths and global settings

---
+++ bash-scripts/compressroms-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ================ Should point to help =================
+if [[ "$1" == "" ]]; then
+    echo "No argument. Please specify:
+        - "gamecube-input <path>"
+        - "wii-input <path>"
+        - "ps2-input <path>"
+        - "gamecube-ciso-output <path>"
+        - "wii-wbfs-output <path>"
+        - "ps2-zso-output <path>"
+        "
+else
+    sed -i 's/^$1=.*/$1=$2/' /media/games/scripts/compressroms-variables.txt
+
+
