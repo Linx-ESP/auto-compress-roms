9c147102eb22e2de27a4f7c5283bc569194be601
Linx <51830853+Linx-ESP@users.noreply.github.com>
2023-05-15 18:03:23 +0200

    Fix folder creation

--- bash-scripts/compressroms-setup.sh
+++ bash-scripts/compressroms-setup.sh
@@ -18,17 +18,17 @@ mkdir -p gc wii ps2
 
 cd "$romsoutputvar"
 mkdir -p \
-"Nintendo GameCube"\
-"Nintendo Wii"\
-"PlayStation 2/OpenPS2Loader/APPS"\
-"PlayStation 2/OpenPS2Loader/ART"\
-"PlayStation 2/OpenPS2Loader/CD"\
-"PlayStation 2/OpenPS2Loader/CFG"\
-"PlayStation 2/OpenPS2Loader/CHT"\
-"PlayStation 2/OpenPS2Loader/DVD"\
-"PlayStation 2/OpenPS2Loader/LNG"\
-"PlayStation 2/OpenPS2Loader/POPS"\
-"PlayStation 2/OpenPS2Loader/THM"\
+"Nintendo GameCube" \
+"Nintendo Wii" \
+"PlayStation 2/OpenPS2Loader/APPS" \
+"PlayStation 2/OpenPS2Loader/ART" \
+"PlayStation 2/OpenPS2Loader/CD" \
+"PlayStation 2/OpenPS2Loader/CFG" \
+"PlayStation 2/OpenPS2Loader/CHT" \
+"PlayStation 2/OpenPS2Loader/DVD" \
+"PlayStation 2/OpenPS2Loader/LNG" \
+"PlayStation 2/OpenPS2Loader/POPS" \
+"PlayStation 2/OpenPS2Loader/THM" \
 "PlayStation 2/OpenPS2Loader/VMC"
 
 echo Might need relogin. You are ready to run compressroms.
